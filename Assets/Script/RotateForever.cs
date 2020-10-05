a95b045f1ae51256bf69ec2eab4caa2c71e85060
WYM <wymm0008@gmail.com>
2020-10-05 16:15:55 +0800

    物体自动旋转

---
+++ Assets/Script/RotateForever.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateForever : MonoBehaviour
+{
+
+    public float speedUp = 0.5f;
+    public float speedLeft = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(Vector3.up * speedUp);
+        this.transform.Rotate(Vector3.left * speedLeft);
+    }
+}
